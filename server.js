e723d7d6d46251cf9c6be99f9cfacbfe76dfbe71
Nícolas Tozato <ntozato9@gmail.com>
2021-11-05 10:02:31 -0300

    requisitos 1 e 2

--- server.js
+++ server.js
@@ -1,1 +1,44 @@
 // Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+const crypto = require('crypto');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log(`usuário entrou ID: ${socket.id}`);
+  const randomNickName = crypto.randomBytes(8).toString('hex');
+
+  socket.on('disconnect', () => {
+    console.log(`${socket.id} saiu`);
+  });
+
+  socket.emit('randomNickname', randomNickName);
+
+  socket.on('message', (msg) => {
+    console.log(msg);
+    const date = new Date().toLocaleString().replace(/\//g, '-');
+    io.emit('message', `${date} - ${msg.nickname}: ${msg.chatMessage}`);
+  });
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+http.listen(3000, () => {
+  console.log('rodando na 3000');
+});
